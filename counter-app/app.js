4bfb94bf4ba4413ecc2dc572eb853a8f9b78e35c
Randy-RM <randymuhema@gmail.com>
2024-06-28 15:34:21 +0200

    feat : create addToCounter function

--- counter-app/app.js
+++ counter-app/app.js
@@ -2,3 +2,8 @@ const buttonRemove = document.getElementById("btn-remove");
 const buttonAdd = document.getElementById("btn-add");
 const compteur = document.getElementById("compteur");
 const buttonReset = document.getElementById("btn-reset");
+
+function addToCompteur() {
+  const nombre = Number(compteur.innerHTML);
+  compteur.innerHTML = `${nombre + 1}`;
+}

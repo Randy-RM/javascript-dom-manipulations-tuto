571a96fd1f0866ce6ffc33344e2967edb79eb3b9
Randy-RM <randymuhema@gmail.com>
2024-06-28 15:28:04 +0200

    feat : select html elements to handle

---
+++ counter-app/app.js
@@ -0,0 +1,4 @@
+const buttonRemove = document.getElementById("btn-remove");
+const buttonAdd = document.getElementById("btn-add");
+const compteur = document.getElementById("compteur");
+const buttonReset = document.getElementById("btn-reset");

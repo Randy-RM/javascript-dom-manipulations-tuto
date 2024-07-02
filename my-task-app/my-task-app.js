2ca9d2cb06ec4b1d7b64f45fbc3798dad529306d
Randy-RM <randymuhema@gmail.com>
2024-07-02 10:36:42 +0200

    feat : create createDeleteButton function

--- my-task-app/my-task-app.js
+++ my-task-app/my-task-app.js
@@ -5,3 +5,14 @@ const taskList = document.getElementById("taskList");
 const hasOnlySpaces = (value) => {
   return value.trim().length != 0;
 };
+
+const createDeleteButton = () => {
+  const buttonDeleteTaskItem = document.createElement("button");
+  const buttonDeleteText = document.createTextNode("X");
+  buttonDeleteTaskItem.appendChild(buttonDeleteText);
+  buttonDeleteTaskItem.addEventListener("click", (event) => {
+    event.preventDefault();
+    event.target.parentElement.remove();
+  });
+  return buttonDeleteTaskItem;
+};

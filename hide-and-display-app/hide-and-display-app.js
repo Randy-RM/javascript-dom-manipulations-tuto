6df299c8bd89548b5003bd6fe15cbbf1d149cc39
Randy-RM <randymuhema@gmail.com>
2024-07-02 13:59:54 +0200

    feat : create displayToysList function

--- hide-and-display-app/hide-and-display-app.js
+++ hide-and-display-app/hide-and-display-app.js
@@ -1,3 +1,9 @@
 const displayToysListButton = document.getElementById("btn-display");
 const hideToysListButton = document.getElementById("btn-hide");
 const toysList = document.getElementById("toys-list");
+
+function displayToysList() {
+  if (toysList.style.display === "none" || toysList.style.display === "") {
+    toysList.style.display = "block";
+  }
+}

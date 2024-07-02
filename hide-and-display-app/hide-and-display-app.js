34c87b77d3d265b80f5a1832be3234f07f412c4b
Randy-RM <randymuhema@gmail.com>
2024-07-02 12:34:18 +0200

    feat : select html elements to handle

---
+++ hide-and-display-app/hide-and-display-app.js
@@ -0,0 +1,3 @@
+const displayToysListButton = document.getElementById("btn-display");
+const hideToysListButton = document.getElementById("btn-hide");
+const toysList = document.getElementById("toys-list");

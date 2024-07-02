448313d673206f3fd15680d2917771a8f79f5d49
Randy-RM <randymuhema@gmail.com>
2024-07-02 10:31:21 +0200

    feat : create hasOnlySpaces function

--- my-task-app/my-task-app.js
+++ my-task-app/my-task-app.js
@@ -1,3 +1,7 @@
 const inputTask = document.getElementById("task");
 const addTaskButton = document.getElementById("addTaskButton");
 const taskList = document.getElementById("taskList");
+
+const hasOnlySpaces = (value) => {
+  return value.trim().length != 0;
+};
